e1235e95b091c1fbd65f8c7420dddfc0e755297d
Alfredo08 <alfredosv08@gmail.com>
2024-07-01 08:06:58 -0600

    Axios

---
+++ Semana 8/Día 1/proyecto-pokemon/src/componentes/DetallePokemon/DetallePokemon.jsx
@@ -0,0 +1,13 @@
+
+const DetallePokemon = (props) => {
+    return(
+        <div>
+            <h2> Nombre: {props.infoPokemon.name} </h2>
+            <p> Altura: {props.infoPokemon.height} </p>
+            <p> Anchura: {props.infoPokemon.weight} </p>
+            <img src={props.infoPokemon.sprites.other.dream_world.front_default} />
+        </div>
+    );
+}
+
+export default DetallePokemon;
